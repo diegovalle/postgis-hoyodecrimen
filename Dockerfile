40f50faa925ce3bba708117066ebca12ca22cdcf
diego <git@diegovalle.net>
2022-06-26 14:34:22 -0500

    Add backup and hoyodecrimen users

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,12 @@
 FROM postgis/postgis:14-3.2
 ENV POSTGRES_USER=deploy
 ENV POSTGRES_DB=apihoyodecrimen
-# POSTGRES_PASSWORD=
+COPY init.sh /docker-entrypoint-initdb.d/
+RUN chown 999 -R /var/lib/postgresql/data
+USER 999
+# ENV POSTGRES_PASSWORD=
+# ENV BACKUP_USER_PASSWORD=
+# ENV HOYODECRIMEN_USER_PASSWORD=
+# TCP 5432
+# VOLUME-MOUNT VOLUME-MOUNT pgdata-ko89b2 /var/lib/postgresql/data
+# CHANGE_OWNER CHOWN /pgdata-ko89b2 999
---
+++ init.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e
+
+## Add read-only user for hoyodecrimen
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+     CREATE ROLE hoyodecrimen WITH LOGIN PASSWORD '$HOYODECRIMEN_USER_PASSWORD'
+     NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION VALID UNTIL 'infinity';
+
+     GRANT CONNECT ON DATABASE apihoyodecrimen TO hoyodecrimen;
+     GRANT USAGE ON SCHEMA public TO hoyodecrimen;
+     GRANT SELECT ON ALL TABLES IN SCHEMA public TO hoyodecrimen;
+     GRANT SELECT ON ALL SEQUENCES IN SCHEMA public TO hoyodecrimen;
+
+     ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO hoyodecrimen;
+
+EOSQL
+
+## Add a read-only user for making backups
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+     CREATE ROLE hoyodecrimen_backup WITH LOGIN PASSWORD '$BACKUP_USER_PASSWORD'
+     NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION VALID UNTIL 'infinity';
+
+     GRANT CONNECT ON DATABASE apihoyodecrimen TO hoyodecrimen_backup;
+     GRANT USAGE ON SCHEMA public TO hoyodecrimen_backup;
+     GRANT SELECT ON ALL TABLES IN SCHEMA public TO hoyodecrimen_backup;
+     GRANT SELECT ON ALL SEQUENCES IN SCHEMA public TO hoyodecrimen_backup;
+
+     ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO hoyodecrimen_backup;
+
+EOSQL
